54e0fa211210024cf7eb6578f4b44a98152671e8
BK-Devices <91716644+BK-Devices@users.noreply.github.com>
2021-10-01 14:45:22 +0530

    Update bomber.py

--- bomber.py
+++ bomber.py
@@ -374,7 +374,7 @@ except FileNotFoundError:
 
 
 __VERSION__ = get_version()
-__CONTRIBUTORS__ = ['BK-Devices]
+__CONTRIBUTORS__ = ['BK-Devices']
 
 ALL_COLORS = [Fore.GREEN, Fore.RED, Fore.YELLOW, Fore.BLUE,
               Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
